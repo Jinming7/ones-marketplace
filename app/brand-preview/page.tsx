462259a7a607179fef262eda96117633387dd0a7
Jinming7 <411263626@qq.com>
2026-02-17 01:28:08 +0800

    chore: clear repository contents

--- app/brand-preview/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BrandPreviewContent } from "@/components/brand/brand-preview-content";
-
-export default function BrandPreviewPage() {
-  return (
-    <div className="mx-auto w-full max-w-5xl py-4">
-      <BrandPreviewContent />
-    </div>
-  );
-}
